bd5c25d94cfe00b464f8aa1bca67a94372f5044b
helgiisaevar <helgiisaevar@gmail.com>
2021-01-20 22:02:54 +0000

    Updates

---
+++ precache-manifest.871369e7edb585354e89e399a5f0244b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36caf92e72f6711c3301f7d5f4fdcf5c",
+    "url": "/tech/index.html"
+  },
+  {
+    "revision": "574e109f2db4f32b791f",
+    "url": "/tech/static/css/main.18840191.chunk.css"
+  },
+  {
+    "revision": "d69d7ba7345c32293bea",
+    "url": "/tech/static/js/2.c4292a38.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/tech/static/js/2.c4292a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574e109f2db4f32b791f",
+    "url": "/tech/static/js/main.d7c4ea36.chunk.js"
+  },
+  {
+    "revision": "7fd9aaa91b6c436525a9",
+    "url": "/tech/static/js/runtime-main.b7d3fc31.js"
+  }
+]);
